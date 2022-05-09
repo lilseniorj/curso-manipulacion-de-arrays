e093b43f9e3b8f4ef9d3c4a5cfe8cca4964f9fe6
Jesus Vargas <jesusvarguer18@gmail.com>
2022-05-09 16:55:40 -0500

    añandiendo nuevo archivo 04-filter

---
+++ server/04-filter.js
@@ -0,0 +1,56 @@
+const { query } = require("express");
+
+const words = ["spary", "limit", "elite", "exuberant"];
+
+const newArray = [];
+for (let index = 0; index < words.length; index++) {
+  const item = words[index];
+  if (item.length >= 6) {
+    newArray.push(item);
+  }
+}
+console.log("newArray", newArray);
+console.log("original", words);
+const rta = words.filter(item => item.length >= 6);
+console.log("rta", rta);
+console.log("original", words);
+
+
+const orders = [
+  {
+    customerName: "Nicolas",
+    total: 60,
+    delivered: true,
+  },
+  {
+    customerName: "Zulema",
+    total: 120,
+    delivered: false,
+  },
+  {
+    customerName: "Santiago",
+    total: 180,
+    delivered: true,
+  },
+  {
+    customerName: "Valentina",
+    total: 240,
+    delivered: true,
+  },
+  {
+    customerName: "Nicolas",
+    total: 2220,
+    delivered: false,
+  },
+];
+const rta3 = orders.filter(item => item.delivered && item.total >= 100);
+console.log("rta3", rta3);
+
+const search = (query) => {
+  return orders.filter(item => {
+    return item.customerName.includes(query);
+  })
+}
+
+// console.log(search("Nico"));
+console.log(search("asdasd"));
